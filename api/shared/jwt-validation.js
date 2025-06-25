df49b1db3db62d2c5caba8d3aa8f370cf9e063f8
jbailliefdi <j.baillie@fdintelligence.co.uk>
2025-06-25 15:24:56 +0100

    testing auth secrets

---
+++ api/shared/jwt-validation.js
@@ -0,0 +1,15 @@
+{
+  "name": "fdi-chatbot-api",
+  "version": "1.0.0",
+  "description": "FDI Chatbot API Functions",
+  "main": "index.js",
+  "dependencies": {
+    "@azure/cosmos": "^4.0.0",
+    "jsonwebtoken": "^9.0.2",
+    "jwks-rsa": "^3.0.1"
+  },
+  "scripts": {
+    "start": "func start",
+    "test": "echo \"No tests specified\""
+  }
+}
